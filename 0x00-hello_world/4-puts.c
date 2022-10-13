8b014c1438433b274d13326e1e7e20a4922e3788
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-13 16:21:54 -0700

    fix4-puts.c betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print using puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
