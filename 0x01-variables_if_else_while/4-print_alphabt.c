105d8de61ac9dfec3795c8c3f88aef708c38c4e0
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-14 22:08:01 -0700

    rints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Description: print all the alphabet in lowcase
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int l = 26;
+
+	for (; i < l; i++)
+	{
+		char let = letters[i];
+
+		if (let != 'e' || let != 'q')
+			putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
