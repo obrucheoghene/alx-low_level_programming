93422d20edf20df1c24b3523860e369a33cf71a5
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-17 17:23:44 -0700

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry level
+ *
+ * Description: Print all possible different combinations of two digits
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -9,3 +9,4 @@
 7-print_tebahpla.c: prints the lowercase alphabet in reverse,
 8-print_base16.c: prints base 16 numbers in lowercase
 9-print_comb.c:prints all possible combinations of single-digit numbers.
+100-print_comb3.c: prints all possible different combinations of two digits
