a0fcc3979dcca64683bb53dc76d55334f374854b
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-19 20:47:13 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit: print last digits
+ * Description: print last digits
+ * @n: number
+ * Return: number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
