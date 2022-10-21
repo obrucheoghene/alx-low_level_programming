1c1e4848612581a76f0e57329aaf19fab1589b7e
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-21 16:28:15 -0700

    multiply number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - product of two numbers
+* @a: num1
+* @b: num2
+* Return: product
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0-isupper.c: checks for uppercase character.
+mul : products of this number 
