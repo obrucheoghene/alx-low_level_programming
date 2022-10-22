63b9f7ad44ca010bad0a0a5c1d64b5951de34c1d
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-21 17:57:57 -0700

    print 10 times a sequence of number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - print 10 times the number from 0 to 14 
+* Return: void
+*/
+void more_numbers(void)
+{
+	int t;
+	int n;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n <= 14; n++)
+			_putchar(n + '0');
+		_putchar('\n');
+	}
+}
+
