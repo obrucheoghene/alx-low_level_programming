c064785672359e4206a1f61e7ce1353443bdda4d
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-21 17:42:39 -0700

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_numbers - print all number
+* Return: no value
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			;
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
