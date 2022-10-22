b400a609624ea785e16917cd4e1d5314ac7b659d
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-21 18:03:29 -0700

    excellent

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* more_numbers - print 10 times the number from 0 to 14 
+* more_numbers - print 10 times the number from 0 to 14
 * Return: void
 */
 void more_numbers(void)
