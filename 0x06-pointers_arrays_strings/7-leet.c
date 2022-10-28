ebb18628858c9c298590ba3d29b1034c4fd14719
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-27 20:22:23 -0700

    meet requirement used array

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,20 +11,18 @@
 char *leet(char *s)
 {
 	int i;
+	int j;
+	char c[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int l[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = 52;
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = 51;
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = 48;
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = 55;
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = 49;
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == c[j])
+				s[i] = l[j];
+		}
 		i++;
 	}
 	return (s);
