37ef402829990091b8b38898cc83b8d7ec78d671
wilfredcloud <wilfredcloudspace@gmail.com>
2022-10-27 19:35:15 -0700

    capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @s: string arg
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (i > 0)
+			{
+				if (s[i - 1] == ' ' ||
+					s[i - 1] == '\t' ||
+					s[i - 1] == '\n' ||
+					s[i - 1] == ',' ||
+					s[i - 1] == ';' ||
+					s[i - 1] == '.' ||
+					s[i - 1] == '!' ||
+					s[i - 1] == '?' ||
+					s[i - 1] == '(' ||
+					s[i - 1] == ')' ||
+					s[i - 1] == '{' ||
+					s[i - 1] == '}')
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@
 3-strcmp.c: compare string
 4-rev_array.c: reverse array
 5-string_toupper.c: changes all lowercase letters of a string to uppercase.
+6-cap_string.c: capitalizes all words of a string.
