d0c3d1535c23a04ff4021176ad70f4c738e8e8f6
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-02 15:02:18 -0700

    returns power of x by y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: number
+ * @y: power
+ *
+ * Return: value of x power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
