c19ca02c48e54519163dde2504631d1b44daf5ec
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-04 20:34:26 -0700

    sum programs argument

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - entry level
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	}
 	while (i < argc)
 	{
-		if (atoi(argv[i]))
+		if (isdigit(*argv[i]))
 			sum += atoi(argv[i]);
 		else
 		{
