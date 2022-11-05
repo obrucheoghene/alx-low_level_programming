d840b44d4d48c8e172267a33c96beacd2b4eb832
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-04 18:54:35 -0700

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry level
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: always 0 success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
