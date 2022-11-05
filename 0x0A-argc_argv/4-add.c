8618e026e8aa6e0226bb2efe5db8a968866b8185
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-04 20:14:11 -0700

    sum programs argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry level
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (atoi(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
