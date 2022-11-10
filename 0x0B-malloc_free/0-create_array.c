cbd16aab7b177205c024145f69d68556af66c4f9
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-09 20:33:22 -0800

    creates array of specific size reserved with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	str = malloc(sizeof(char) * size);
-	if (str == NULL)
+	if (str == NULL || size == 0)
 	{
 		str = NULL;
 		return (str);
