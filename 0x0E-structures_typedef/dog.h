d02ebc0d4cb2581ebc9e8097198285a95fb654c2
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-23 13:20:56 -0800

    fix betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 
