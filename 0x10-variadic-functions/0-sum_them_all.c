f61f9099b92fbf5b003f21fbec74f5796bb8d9e4
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-23 02:00:15 -0800

    creates sum_them_all

---
+++ 0x10-variadic-functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all args
+ *
+ * @n: number of args
+ * @...: variadic args
+ *
+ * Return: sum of all args
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i;
+	int sum = 0;
+	va_list ap; /*initialize arg list*/
+
+	/*if n == 0 return 0*/
+	if (n == 0)
+		return (0);
+	/*start arg list */
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	return (sum);
+}
