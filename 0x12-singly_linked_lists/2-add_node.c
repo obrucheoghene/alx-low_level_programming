1ca7b20310747b11b7cc752051cb97fbdc43b286
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-24 20:19:25 -0800

    adds node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,26 +13,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new;
+	char *dup;
+
 	
-
-	if (*head == NULL)
-	{
-		*head = malloc(sizeof(list_t));
-		(*head)->next = NULL;
-		(*head)->str = strdup(str);
-		(*head)->len = strlen(str);
-		return (*head);
-	}
-
 	new = malloc(sizeof(list_t));	
-
 
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	dup = strdup(str);
+	if(dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+
+	new->str = dup;
 	new->len = strlen(str);
 	new->next = *head;
+
+	*head = new;
 
 	return (new);
 }
