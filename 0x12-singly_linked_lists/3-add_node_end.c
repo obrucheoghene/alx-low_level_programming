d0469186e6c9b21348ea1c2cd2a71fc47d295463
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-24 20:43:36 -0800

    fixinf

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,8 +16,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp;
 
 	if (new == NULL || dup == NULL)
+	{
+		free(new);
 		return (NULL);
-
+	}
 	new->str = dup;
 	new->len = strlen(str);
 	new->next = NULL;
