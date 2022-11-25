679a2db809baba34ddb73d9b85cae42bcb961adb
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-24 20:21:33 -0800

    fix bety

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,14 +15,14 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	char *dup;
 
-	
-	new = malloc(sizeof(list_t));	
+
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
-	if(dup == NULL)
+	if (dup == NULL)
 	{
 		free(new);
 		return (NULL);
