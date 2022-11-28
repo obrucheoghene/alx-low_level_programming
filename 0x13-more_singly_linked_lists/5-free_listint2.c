b39a57887ad7721ff8c68071cbe704c60233c077
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-28 12:33:32 -0800

    frees list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while ((*head)->next != NULL)
+	while (*(*head) != NULL)
 	{
 		temp = *head;
 		head = &(*head)->next;
 		free(temp);
 	}
-	*head = NULL;
 
 }
 
