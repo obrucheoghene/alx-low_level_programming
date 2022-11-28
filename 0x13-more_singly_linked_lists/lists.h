52c5e1eaaae38f9dfe55664394115e662e6d9cbe
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-28 04:25:42 -0800

    adds node at the beginning

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -24,5 +25,8 @@ int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 
 #endif
