a5882dd7b540ceca1a5c2bd8f8cd688f85df7d78
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-28 13:28:38 -0800

    pops list head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop list node
+ * @head: pointer to head pointer
+ *
+ * Return: head data or 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int n = 0;
+
+	if (temp != NULL)
+	{
+		/* move to the next node to become head */
+		*head = (*head)->next;
+		n = temp->n;
+	}
+	return (n);
+
+}
