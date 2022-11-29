8b02abd7f0a3ccd49ed464e2260f696087a472f7
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-28 16:05:06 -0800

    insert node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node at index
+ * @head: pointer to head pointer
+ * @idx: index
+ * @n: new node data
+ *
+ * Return: address of new node or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *pre;
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new != NULL && *head != NULL)
+	{
+		while (i < idx && *head != NULL)
+		{
+			pre = *head;
+			head = &(*head)->next;
+		}
+
+		if (i == idx && head != NULL)
+		{	
+			new->n = n;
+			pre->next = new;
+			new->next = *head;
+		}
+	}
+	return (NULL);
+}
