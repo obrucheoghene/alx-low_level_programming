2e0e9a2687f30e098b72bd393bfba43483414f58
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-29 20:05:08 -0800

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse listint
+ * @head: pointer to head pointer
+ *
+ * Return: return pointer to reverse head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+
+}
+
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,4 +34,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+
+/*advance*/
+listint_t *reverse_listint(listint_t **head);
+listint_t *reverse_recursion(listint_t *head, listint_t *rev_head);
 #endif
