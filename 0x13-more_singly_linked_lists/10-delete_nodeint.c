4dbe273fd3f81ef49457f30604a375fb4e1dbf6e
wilfredcloud <wilfredcloudspace@gmail.com>
2022-11-29 17:27:42 -0800

    removes trailing whitespace

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 	if (i == index && node_at_index != NULL)
-	{	
+	{
 		/*if index is 0*/
 		if (index == 0)
 			*head = node_at_index->next;
