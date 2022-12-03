90c035664343841b5ea2992bb8d3ad81de08ec7a
wilfredcloud <wilfredcloudspace@gmail.com>
2022-12-02 18:58:09 -0800

    print biinary

--- 0x13-more_singly_linked_lists/101-main2.c
+++ 0x13-more_singly_linked_lists/101-main2.c
@@ -10,7 +10,9 @@
  */
 int main(void)
 {
+    listint_t *head;
     listint_t *head2;
+    listint_t *node;
 
     head2 = NULL;
     add_nodeint(&head2, 0);
@@ -22,6 +24,15 @@ int main(void)
     add_nodeint(&head2, 402);
     add_nodeint(&head2, 1024);
     print_listint_safe(head2);
-    
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
     return (0);
 }
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,1 +1,24 @@
+#include "lists.h"
 
+
+/**
+ * print_listint_safe - prints all the elements of a singly list
+ * @head: pointer to singly list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int n = 0;
+	const listint_t *temp = head;
+
+	if (head == NULL)
+		return (98);
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		n++;
+	}
+	return (n);
+}
