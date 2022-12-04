820d49b208cb3f1de0004fd4431ecad4a1b01521
wilfredcloud <wilfredcloudspace@gmail.com>
2022-12-04 13:58:08 -0800

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
+	char *name2, *owner2;
 
 	if (my_dog == NULL)
+	{
 		return (NULL);
+		free(my_dog);
+	}
+
+	name2(name2, name);
+	owner2(owner2, owner);
+	
 
 	my_dog->age = age;
 	my_dog->owner = owner;
 	my_dog->name = name;
+
+
 
 	return (my_dog);
 }
