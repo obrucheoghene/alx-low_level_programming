599e48ec640aa6f4d095bfdb92947c6c05c3cc91
wilfredcloud <wilfredcloudspace@gmail.com>
2022-12-04 14:03:58 -0800

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
-	char *name2, *owner2;
+	char *name2 = malloc(sizeof(char) * strlen(name) + 1);
+	char *owner2 = malloc(sizeof(char) * strlen(owner) + 1);
 
 	if (my_dog == NULL)
 	{
@@ -19,8 +20,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(my_dog);
 	}
 
-	name2(name2, name);
-	owner2(owner2, owner);
+	strcpy(name2, name);
+	strcpy(owner2, owner);
 	
 
 	my_dog->age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - dog struct
