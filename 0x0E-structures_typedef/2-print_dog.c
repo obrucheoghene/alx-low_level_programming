9245925046d4a1a16eb09ef78339172d409bfa2f
wilfredcloud <wilfredcloudspace@gmail.com>
2022-12-04 13:09:05 -0800

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name)
+	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-	if (d->age)
+	if (d->age > 0)
 		printf("Age: %f\n", d->age);
 	else
-		printf("(nil)\n");
-	if (d->owner)
+		printf("Age: (nil)\n");
+	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 
 }
