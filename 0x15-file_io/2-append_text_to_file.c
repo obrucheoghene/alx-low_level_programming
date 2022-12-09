4c0d57d1b6cdbc8755f09e567c86776043bd69b0
wilfredcloud <wilfredcloudspace@gmail.com>
2022-12-09 14:51:01 -0800

    appends text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,6 +18,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	if (write(fd, text_content, strlen(text_content)) < 0)
 		return (-1);
 
