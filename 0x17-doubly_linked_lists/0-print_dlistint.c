6ad5c7e36722378b809808711d304237fe502606
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 22:55:15 +0100

    Add print_dlistint to print all elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+/**
+ * print_dlistint - Print all the elements of a dlistint_t list
+ * @h: pointer to the header
+ *
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t n = 0;
+
+	/* Check if list is empty*/
+	if (!temp)
+		return (n);
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		n++;
+		temp = temp->next;
+	}
+
+	return (n);
+}
