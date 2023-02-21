8ecb06dbf649ebfbb78bf2adc5c006d1e8fecf40
Wilfred <wilyfrez@gmail.com>
2023-02-21 22:37:37 +0100

    Update function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,5 +7,18 @@
  */
 void print_times_table(int n)
 {
-	
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+				printf("%d,", i * j);
+			else if (j == n)
+				printf("%3d\n" i * j);
+			else
+				printf("%3d," i * j);
+		}
+	}
 }
