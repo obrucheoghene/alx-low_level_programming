3536df3e7ef33194f9e648ab34816302e561b649
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 22:19:25 +0100

    Add function to concat n of string

---
+++ 0x18-dynamic_libraries/strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strncat - concat number of string
+ * @dest: destination
+ * @src: source
+ * @n: nunber
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
