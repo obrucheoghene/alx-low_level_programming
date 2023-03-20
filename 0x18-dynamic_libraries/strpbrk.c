3c33e008f6b66b6ce1757a6551c93f21a3392382
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 22:37:24 +0100

    Add function to finds the first character in  str

---
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strpbrk - find character in string
+ * @s: string
+ * @accept: accept
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+}
---
+++ 0x18-dynamic_libraries/strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strstr - searches a substring in a string
+ * @haystack: haystack
+ * @needle: needle
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
