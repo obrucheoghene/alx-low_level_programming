e9c2edb9edabdc6dc4dc7b14812a36606c9fbdd5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 16:48:47 -0700

    Add C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add number
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * sub - add number
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
