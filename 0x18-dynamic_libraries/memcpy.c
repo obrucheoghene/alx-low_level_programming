4e3f0f7ead46fd00e78a3ff7102ff290c46e9ae1
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 22:34:59 +0100

    Add function for memory copy

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
