66a1f729a8b7da99d6860ae1965e0bf02b637e45
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 16:00:19 -0700

    Update script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-for file in *.c; do
-    if [[ -f "$file" ]]; then
-        gcc -c -fPIC "$file"
-    fi
-done
-
-gcc -shared -o liball.so "$file"
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
  
