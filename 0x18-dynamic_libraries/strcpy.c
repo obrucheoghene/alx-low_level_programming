a73e702d48276f6faf64192ec942ec35f4232056
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 22:19:12 +0100

    Add function to copy string

---
+++ 0x18-dynamic_libraries/strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string from src to dest
+ * @dest: destination
+ * @src: source
+ * Return: pointer to string
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
