f11a0fc2d36ae0c550d79da331c6cc45943a4061
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-20 22:19:40 +0100

    Add function to copy of string

---
+++ 0x18-dynamic_libraries/strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a number of character
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
+
