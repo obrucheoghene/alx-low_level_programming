6929d59de6755e21e3aca1cdaf7b3bdc00bd25b4
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-08 03:52:25 +0100

    Implement hash function

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: Hash item Key
+ * @size: Size of hash table
+ * Return: Index for the key/value pair
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	hash = hash_djb2(key);
+	return (hash % size);
+}
