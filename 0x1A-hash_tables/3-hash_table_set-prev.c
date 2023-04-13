d4e7c36a7b67981871f163534950d9471f992c55
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-13 04:32:29 +0100

    set hash table

---
+++ 0x1A-hash_tables/3-hash_table_set-prev.c
@@ -0,0 +1,56 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key of the item to be added
+ * @value: The value of the item to be added
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int position;
+	hash_node_t *new_node, *current_node;
+	char *value_dup;
+
+
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_dup = strdup(value);
+        key_dup = strdup(key);
+	if (value_dup == NULL || key_dup == NULL)
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+        {
+                free(value_dup);
+		return (0);
+        }
+
+	new_node->key = key_dup;
+	new_node->value = value_dup;
+	new_node->next = NULL;
+
+	position = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[position];
+
+	if (current_node == NULL)
+		ht->array[position] = new_node;
+	else
+	{
+		if (current_node->key == key)
+			current_node->value = value_dup;
+		else
+		{
+			new_node->next = current_node;
+			current_node = new_node;
+		}
+	}
+	return (1);
+}
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,45 +8,46 @@
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int position;
-	hash_node_t *new_node, *current_node;
-	char *value_dup;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	if (key == NULL || *key == '\0' || value == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
-	value_dup = strdup(value);
-	if (value_dup == NULL)
-		return (0);
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = value_dup;
-	new_node->next = NULL;
-
-	position = key_index((const unsigned char *)key, ht->size);
-	current_node = ht->array[position];
-
-	if (current_node == NULL)
-		current_node = new_node;
-	else
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (current_node->key == key)
-			current_node->value = value_dup;
-		else
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			new_node->next = current_node;
-			current_node = new_node;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
